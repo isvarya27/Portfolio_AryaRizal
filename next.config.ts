2eea83f5f7e3d406e7e64881c524e7356a25372b
Arya Rizal <aryachubby27@gmail.com>
2025-10-23 15:35:15 +0700

    add progress

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const repoName = "Portfolio_AryaRizal";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   images: {
-    unoptimized: true,
+    unoptimized: true, // wajib diaktifkan untuk GitHub Pages
   },
+  trailingSlash: true, // tambahan ini opsional tapi direkomendasikan
 };
 
 export default nextConfig;
