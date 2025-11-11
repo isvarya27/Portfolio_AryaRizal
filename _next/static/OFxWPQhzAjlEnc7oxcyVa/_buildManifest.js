c4431e5c0d6132d69127b20922fd515344642ef0
Arya Rizal <aryachubby27@gmail.com>
2025-11-11 23:50:19 +0700

    Updates

--- _next/static/OFxWPQhzAjlEnc7oxcyVa/_buildManifest.js
+++ _next/static/OFxWPQhzAjlEnc7oxcyVa/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Portfolio_AryaRizal//_next/:path+",
-        "destination": "/Portfolio_AryaRizal/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
